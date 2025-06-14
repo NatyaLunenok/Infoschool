01fe8e039bb9a3471776d8c5c72921dd2539c088
mas117 <sitnikovamasha2004@gmail.com>
2025-06-14 20:25:54 +0700

    logic pages

--- src/Layout/Header/FirstLine/FirstLine.jsx
+++ src/Layout/Header/FirstLine/FirstLine.jsx
@@ -1,18 +1,68 @@
-import React, { useState, useEffect, useCallback } from 'react'; // <-- Добавлен useCallback
+import React, { useEffect } from 'react';
 import styles from './FirstLine.module.css';
 import logo from '../../../images/logo.png';
+import FetchWithAuth from '../../../Pages/Authorization/FetchWithAuth';
+import { useNavigate } from "react-router-dom";
 
 const FirstLine = () => {
-    return(
-    <div className={styles.ConteinerFirstLine}>
-        <div className={styles.logo}>
-            <img src={logo} alt="" />
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        if (!refreshToken) {
+            console.warn('No refresh token found. Cannot logout.');
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            localStorage.removeItem('username');
+            localStorage.removeItem('role');
+            navigate('/');
+            return;
+        }
+
+        try {
+            const data = await FetchWithAuth('http://127.0.0.1:8000/diary/logout/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ refresh: refreshToken }),
+            });
+             console.log("выход",data);
+
+            if (!data ) {
+                console.error('Logout failed:', data);
+            } else {
+                console.log('Logout successful');
+            }
+        } catch (error) {
+            console.error('Error during logout:', error);
+        } finally {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            localStorage.removeItem('username');
+            localStorage.removeItem('role');
+            navigate('/');
+        }
+    };
+
+    useEffect(() => {
+      const data =   localStorage.getItem('username');
+        console.log("юзер",data);
+    }, []);
+
+    return (
+        <div className={styles.ConteinerFirstLine}>
+            <div className={styles.logo}>
+                <img src={logo} alt=""/>
+            </div>
+            <div className={styles.textbox}>
+                <p className={styles.NameUser}>Учитель</p>
+                <button className={styles.ButtonExit} onClick={handleLogout}>ВЫХОД</button>
+            </div>
         </div>
-        <div className={styles.textbox}>
-            <p className={styles.NameUser}>Учитель</p>
-            <button className={styles.ButtonExit}>ВЫХОД</button>
-        </div>
-    </div>
     );
 };
+
 export default FirstLine;
+
