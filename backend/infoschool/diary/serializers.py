dc1b2042bdc93f3bafbc897008c200e553292695
Nastya Lunenok <nastya.lunenok@gmail.com>
2025-06-15 23:27:24 +0700

    Edit TeacherSerializer

--- backend/infoschool/diary/serializers.py
+++ backend/infoschool/diary/serializers.py
@@ -333,13 +333,13 @@ class ParentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Parent
-        fields = ['last_name', 'first_name', 'patronymic', 'phone_number', 'type_name']
+        fields = ['id', 'last_name', 'first_name', 'patronymic', 'phone_number', 'type_name']
 
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
-        fields = ['last_name', 'first_name', 'patronymic', 'email', 'phone_number']
+        fields = ['id', 'last_name', 'first_name', 'patronymic', 'email', 'phone_number']
 
 
 class StudentSerializer(serializers.ModelSerializer):
